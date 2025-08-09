4047a04bdf130b922e7d99d335e587fa23e50787
“Gonzalo” <gonzalos.camara@gmail.com>
2025-08-09 13:29:30 +0200

    Gym Project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Welcome from './components/pages/Welcome.vue';
+import Workout from './components/pages/Workout.vue';
+import Dashboard from './components/pages/Dashboard.vue';
+import Layout from './components/layouts/Layout.vue';
+
+const selectedDisplay = 3;
+</script>
+
+<template>
+  <Layout>
+    <Welcome v-if="selectedDisplay === 1" />
+    <Dashboard v-if="selectedDisplay === 2" />
+    <Workout v-if="selectedDisplay === 3" />
+  </Layout>
+</template>
+
+<style scoped></style>
---
+++ src/components/pages/Dashboard.vue
@@ -0,0 +1,47 @@
+<script setup>
+import Grid from '../Grid.vue';
+import { gymHealthFacts } from '../../utils/index';
+
+const randomNumber = Math.floor(Math.random() * gymHealthFacts.length);
+</script>
+
+<template>
+  <section id="dashboard">
+    <div class="card tip-container">
+      <h2>Welcome user</h2>
+      <p class="tip">
+        <strong>Daily Tip</strong> <br />
+        {{ gymHealthFacts[randomNumber] }}
+      </p>
+      <button>Start workout &rarr;</button>
+    </div>
+    <Grid />
+  </section>
+</template>
+
+<style scoped>
+.tip-container,
+.tip-container div,
+#dashboard {
+  display: flex;
+}
+
+.tip-container,
+#dashboard {
+  flex-direction: column;
+}
+
+#dasboard {
+  gap: 2rem;
+}
+
+.tip-container {
+  gap: 0.5rem;
+}
+
+@media (min-width: 640px) {
+  .tip-container {
+    gap: 1rem;
+  }
+}
+</style>
